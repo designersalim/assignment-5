515a5fdc7ebe73572ad8530ccaad91adcb32bd5b
designersalim <designer.bd.salim@gmail.com>
2024-09-24 01:41:37 +0600

    java loaded

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ document.getElementById('donate-btn').addEventListener('click', function(){
     document.getElementById('donate-btn').classList.remove('bg-lime-400');
     document.getElementById('history-btn').classList.remove('bg-lime-400');
     document.getElementById('donate-btn').classList.add('bg-lime-400')
+    window.location.href = './index.html'
 
 })
 
@@ -25,7 +26,7 @@ document.getElementById('noakhali-btn').addEventListener('click', function(event
     event.preventDefault();
     const noakhaliValue = getInputElementById('noakhali-input')
     const balance = getTextElementById('current-balance')
-    if(noakhaliValue > 0 && noakhaliValue < balance){
+    if(noakhaliValue >= 0 && noakhaliValue <= balance){
         const noakhaliBalance = getTextElementById('noakhali-balance');
         const numText = parseFloat(noakhaliBalance);
         const newBalance = noakhaliValue + numText;
@@ -41,4 +42,41 @@ document.getElementById('noakhali-btn').addEventListener('click', function(event
 
 document.getElementById('close-btn').addEventListener('click', function(){
     document.getElementById('congrats-btn').classList.add('hidden')
+})
+
+document.getElementById('feni-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const feniValue = getInputElementById('feni-input')
+    const balance2 = getTextElementById('current-balance')
+    if(feniValue >= 0 && feniValue <= balance2){
+        const feniBalance = getTextElementById('feni-balance');
+        const numText2 = parseFloat(feniBalance);
+        const newBalance2 = feniValue + numText2;
+        document.getElementById('feni-balance').innerText = newBalance2;
+        document.getElementById('current-balance').innerText = balance2 - feniValue;
+        document.getElementById('congrats-btn').classList.remove('hidden');
+        
+    }
+    else{
+        alert("Please enter correct amount.")
+    }
+})
+
+
+document.getElementById('quota-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const quotaValue = getInputElementById('quota-input')
+    const balance3 = getTextElementById('current-balance')
+    if(quotaValue >= 0 && quotaValue <= balance3){
+        const quotaBalance = getTextElementById('quota-balance');
+        const numText3 = parseFloat(quotaBalance);
+        const newBalance3 = quotaValue + numText3;
+        document.getElementById('quota-balance').innerText = newBalance3;
+        document.getElementById('current-balance').innerText = balance3 - quotaValue;
+        document.getElementById('congrats-btn').classList.remove('hidden');
+        
+    }
+    else{
+        alert("Please enter correct amount.")
+    }
 })
